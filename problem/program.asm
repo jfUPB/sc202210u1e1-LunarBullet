fd509747ca361163e8daec70e251a3a5b03aeba2
LunarBullet <lunarbulletstudios@gmail.com>
2022-02-11 03:25:37 -0500

    Explicacion mejor del codigo (para cuando continue y termine el trabajo)

--- problem/program.asm
+++ problem/program.asm
@@ -9,25 +9,26 @@ M=-1    // A la ubicacion 16384+1 le asignamos valor de -1
 @SCREEN //@16384
 D=A     //guardo en D el valor 16384
 @R1     //accedo a R1
-M=D     //Le doy el valor de 16384
+M=D     //Le doy el valor de 16384 a R1
 @R1 //------- LOOP -------- IF D<24575 go to line 9; A=24575
-A=M     //A = M de R1
-M=-1    //A la ubicacion A = M de R1 le ponemos el valor en memoria de -1
+A=M     //A = M de R1 (basicamente la escencia del contador!)
+M=-1    //a la ubicacion A, que es @R1 le ponemos el valor en memoria de -1
 @R1     //vamos a la ubicacion del contador R1
 M=M+1   //Sumamos +1 Al contador R1
-@R1
-D=A
+@R1     //vamos a la ubicacion @R1
+D=A     //Guardamos en D el valor @R1
 @24576 //quiero lograr: IF D<24576 go to line 9; A=24576
-D=D-A   //D = @R1-24576 = -1
-@12
-D;JLT //if D<0, go to @9
+D=D-A   //D = (la ubicacion almacenada en D, que es @R1)-(24576, que es el valor almacenado en A anterior) = -1
+@12     
+D;JLT //Si D es menor a 0, vamos a @12
 
 // Juan Sebastian Mandon Sierra
 // 000426587
 // juan.mandon@upb.edu.co
+//Mezcle ingles y español (normalmente documento en ingles) espero que no sea problema! 
 
-//Screen is from 16384 to 24575, must fill em' all with -1's
-//Keyboard is 24576
+//Screen is from 16384 to 24575, must fill em' all with -1's so its fully black!
+//Keyboard @ is 24576
 
 
 
