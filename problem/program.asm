15491d8b1d5c50d3404a649c22353f00cdb86149
LunarBullet <lunarbulletstudios@gmail.com>
2022-02-11 02:39:32 -0500

    Experimentacion y llenado full negro dos posiciones de memoria

--- problem/program.asm
+++ problem/program.asm
@@ -1,6 +1,26 @@
 // Juan Sebastian Mandon Sierra
 // 000426587
 // juan.mandon@upb.edu.co
-(START)
-    @START
-    0;JMP
+
+//Screen is from 16384 to 24575, must fill em' all with -1's
+//Keyboard is 24576
+
+
+
+@SCREEN //guardamos a ubicacion 16384 en A
+M=-1    //Asignamos el valor -1 a la ubicacion 16384 en memoria
+@SCREEN //guardamos a ubicacion 16384 en A
+D=A     //Asignamos a D el valor de A (16384)
+@R1     //Ubicacion 1
+M=D     //Asignamos a ubicacion 1 el valor de D (16384)
+A=M+1   //Vamos a la la ubicacion 16384+1
+M=-1    // A la ubicacion 16384+1 le asignamos valor de -1
+
+
+
+
+
+
+
+
+
