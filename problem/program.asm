1dd2f54e4b41a380bee988d4fc05f2f93c0e335f
LunarBullet <lunarbulletstudios@gmail.com>
2022-02-13 03:08:23 -0500

    Update program.asm

--- problem/program.asm
+++ problem/program.asm
@@ -1,14 +1,14 @@
 @SCREEN //Ubicacion en memoria de la pantalla
 D=A     //Guardo la ubicacion de screen en D
-@25     //mi contador, posicion R1 en memoria
+@R1     //mi contador, posicion R1 en memoria
 M=D     //guardo el valor D en A (basicamente inicializo el valor de R1, mi contador, a 16384)
 (LLENARPANTALLALOOP)
-@25 
-A=M
-M=-1
-@25
-M=M+1
-M=D
+@R1     //Posicion 1
+A=M     //Posicion del valor interno de R1
+M=-1    //Al valor interno de R1 le asigno -1
+@R1     //Posicion 1
+M=M+1   //Posicion 1, le sumo 1 al contador.
+D=M     
 @24575
 D=D-A
 @LLENARPANTALLALOOP
