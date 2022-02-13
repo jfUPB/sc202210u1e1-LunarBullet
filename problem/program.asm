37c8d5696d9fa3b274b90b72e6ab6ce84c6b6c09
LunarBullet <lunarbulletstudios@gmail.com>
2022-02-13 04:20:52 -0500

    Codigo funcional altamente optimizado
    Despues de un sinfin de intentos, he logrado optimizar el codigo con la menor cantidad de lineas, adicionalmente se puede presionar F para negro y C para borrar y volver a hacerlo cuantas veces se desee

--- problem/program.asm
+++ problem/program.asm
@@ -5,12 +5,18 @@ D=A     //Guardo la ubicacion de screen en D
 M=D     //guardo el valor D en A (basicamente inicializo el valor de R1, mi contador, a 16384)
 @KBD    //Posicion input del teclado
 D=M     //Almaceno la letra del teclado en memoria
-@INICIO //Posicion inicio
-D;JEQ   //salto a inicio si D es igual a 0 (es decir, si ninguna tecla se presiona) O CONTINUO SI NO ES 0
 @70     //Posicion 70
 D=D-A   //D, que es el valor del teclado, le resto A
 @LLENARPANTALLALOOP //selecciono ubicacion para posible salto
 D;JEQ   //Si D es 0, es decir, si efectivamente presione la letra F, pues D seria 70, y 70-A (A es 70 anteriormente) daria 0
+@KBD    //Posicion input del teclado
+D=M     //Almaceno la letra del teclado en memoria
+@67     //Posicion 67
+D=D-A   //D, que es el valor del teclado, le resto A
+@LIMPIARPANTALLALOOP //selecciono ubicacion para posible salto
+D;JEQ   //Si D es 0, es decir, si efectivamente presione la letra C, pues D seria 67, y 67-A (A es 67 anteriormente) daria 0
+@INICIO
+D;JMP //salto a inicio
 
 (LLENARPANTALLALOOP)
 @R1     //Posicion 1
@@ -18,27 +24,14 @@ A=M     //Posicion del valor interno de R1
 M=-1    //Al valor interno de R1 le asigno -1
 @R1     //Posicion 1
 M=M+1   //Posicion 1, le sumo 1 al contador.
-D=M     
-@24576
+D=M     //D = valor en memoria de R1
+@R1
+D=M
+@24575
 D=D-A
-@LLENARPANTALLALOOP
-D;JLT
-@ROMPERRESETEARLOOP
-D;JMP
-
-(ROMPERRESETEARLOOP)
-//Primero, reseteamos el contador, pues de ser necesario limpiar la pantalla se necesita el contador en la posicion 1, que es SCREEN
-@SCREEN //Ubicacion en memoria de la pantalla
-D=A     //Guardo la ubicacion de screen en D
-@R1     //mi contador, posicion R1 en memoria
-M=D     //le guardo la posicion de screen que esta guardada en D
-@KBD    //accedo a ubicacion en memoria de teclado
-D=M     //guardo en D el valor de la ubicacion KBD
-@67     //A es 67
-D=D-A   //A D le resto 67, si efectivamente es el mismo numero, es decir, presiono 67 (osea tecla C) daria 0 y el loop de ir a limpiar pantalla se efectua
-@LIMPIARPANTALLALOOP
+@INICIO
 D;JEQ
-@ROMPERLOOP
+@LLENARPANTALLALOOP
 D;JMP
 
 (LIMPIARPANTALLALOOP)
@@ -47,12 +40,14 @@ A=M     //Posicion del valor interno de R1
 M=0    //Al valor interno de R1 le asigno -1
 @R1     //Posicion 1
 M=M+1   //Posicion 1, le sumo 1 al contador.
-D=M     
+D=M     //D = valor en memoria de R1
+@R1
+D=M
 @24575
 D=D-A
-@LLENARPANTALLALOOP
-D;JLE
-@ROMPERLOOP
+@INICIO
+D;JEQ
+@LIMPIARPANTALLALOOP
 D;JMP
 
 
