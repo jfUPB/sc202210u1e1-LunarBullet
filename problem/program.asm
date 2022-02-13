25ec3855bd54ade424e75bb07b6acdac80ea830c
LunarBullet <lunarbulletstudios@gmail.com>
2022-02-13 02:43:14 -0500

    no entiendo porque git me da error si sirve???

--- problem/program.asm
+++ problem/program.asm
@@ -1,12 +1,12 @@
 @SCREEN //Ubicacion en memoria de la pantalla
 D=A     //Guardo la ubicacion de screen en D
-@R1 //mi contador, posicion R1 en memoria
+@1 //mi contador, posicion R1 en memoria
 M=D //guardo el valor D en A (basicamente inicializo el valor de R1, mi contador, a 16384)
 (LLENARPANTALLALOOP)
-@R1
+@1
 A=M
 M=-1
-@R1
+@1
 M=M+1
 D=M
 @24575
