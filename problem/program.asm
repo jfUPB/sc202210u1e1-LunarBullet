9613300c7c1c96475deeef5b641de58ffa622aac
LunarBullet <lunarbulletstudios@gmail.com>
2022-02-13 02:22:52 -0500

    Decidi intentar todo desde 0 con mi experiencia obtenida

--- problem/program.asm
+++ problem/program.asm
@@ -1,49 +1,4 @@
-@16384 //@16384
-D=A     //guardo en D el valor 16384
-@R1     //accedo a R1
-M=D     // //Le doy el valor de 16384 a R1
-(BLACKLOOPSTART)
-//Loop de todo negro inicia aca
-   
-@R1     //posicion en memoria 1
-A=M     //A = M de R1 (basicamente la escencia del contador!)
-M=-1    //a la ubicacion A, que es @R1 le ponemos el valor en memoria de -1
-@R1     //vamos a la ubicacion del contador R1
-D=M
-@24576
-D=D-A
-@LOOPBREAKER
-D;JEQ
-@R1
-M=M+1   //Sumamos +1 Al contador R1
-@R1     //vamos a la ubicacion @R1
-D=A     //Guardamos en D el valor @R1
-@24576 //quiero lograr: IF D<24576 go to line BLACKLOOPSTART; A=24576
-D=D-A   //D = (la ubicacion almacenada en D, que es @R1)-(24576, que es el valor almacenado en A anterior) = -1
-@BLACKLOOPSTART     
-D;JLT //Si D es menor a 0, vamos a @BLACKLOOPSTART
 
-
-(BLANKLOOPSTART)
-
-//Loop de todo negro inicia aca
-M=D     //Le doy el valor de 16384 a R1 --start of black fill loop--
-@R3     //posicion en memoria 3
-A=M     //A = M de R1 (basicamente la escencia del contador!)
-M=0    //a la ubicacion A, que es @R1 le ponemos el valor en memoria de -1
-@R1     //vamos a la ubicacion del contador R1
-M=M+1   //Sumamos +1 Al contador R1
-@R1     //vamos a la ubicacion @R1
-D=A     //Guardamos en D el valor @R1
-@24576 //quiero lograr: IF D<24576 go to line BLANKLOOPSTART; A=24576
-D=D-A   //D = (la ubicacion almacenada en D, que es @R3)-(24576, que es el valor almacenado en A anterior) = 0
-@BLACKLOOPSTART     
-D;JLT //Si D es menor a 0, vamos a @BLANKLOOPSTART
-
-(LOOPBREAKER)
-D=0
-@LOOPBREAKER
-D;JEQ
 
 // Juan Sebastian Mandon Sierra
 // 000426587
