f4bc3fca0094b34ee6b579622c2176141554dacb
LunarBullet <lunarbulletstudios@gmail.com>
2022-02-13 02:09:58 -0500

    Trying to do a proper loop breaker

--- problem/program.asm
+++ problem/program.asm
@@ -1,15 +1,19 @@
-
 @SCREEN //@16384
 D=A     //guardo en D el valor 16384
 @R1     //accedo a R1
 (BLACKLOOPSTART)
-
 //Loop de todo negro inicia aca
 M=D     //Le doy el valor de 16384 a R1 --start of black fill loop--
 @R1     //posicion en memoria 1
 A=M     //A = M de R1 (basicamente la escencia del contador!)
 M=-1    //a la ubicacion A, que es @R1 le ponemos el valor en memoria de -1
 @R1     //vamos a la ubicacion del contador R1
+D=M
+@24576
+D=D-A
+@LOOPBREAKER
+D;JEQ
+@R1
 M=M+1   //Sumamos +1 Al contador R1
 @R1     //vamos a la ubicacion @R1
 D=A     //Guardamos en D el valor @R1
@@ -17,9 +21,7 @@ D=A     //Guardamos en D el valor @R1
 D=D-A   //D = (la ubicacion almacenada en D, que es @R1)-(24576, que es el valor almacenado en A anterior) = -1
 @BLACKLOOPSTART     
 D;JLT //Si D es menor a 0, vamos a @BLACKLOOPSTART
-D=0
-@LOOPBREAKER
-D;JEQ
+
 
 (BLANKLOOPSTART)
 
