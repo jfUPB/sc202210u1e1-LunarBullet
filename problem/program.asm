7258ceb0b5b9c26eb91bffee6d33f4a59d7357f0
LunarBullet <lunarbulletstudios@gmail.com>
2022-02-13 02:41:35 -0500

    Full screen black, very clean code!

--- problem/program.asm
+++ problem/program.asm
@@ -1,21 +1,32 @@
-// Juan Sebastian Mandon Sierra
+@SCREEN //Ubicacion en memoria de la pantalla
+D=A     //Guardo la ubicacion de screen en D
+@R1 //mi contador, posicion R1 en memoria
+M=D //guardo el valor D en A (basicamente inicializo el valor de R1, mi contador, a 16384)
+(LLENARPANTALLALOOP)
+@R1
+A=M
+M=-1
+@R1
+M=M+1
+D=M
+@24575
+D=D-A
+@LLENARPANTALLALOOP
+D;JLE
+@ROMPERLOOP
+D;JMP
+
+(ROMPERLOOP)
+@ROMPERLOOP
+D;JMP
+
+
+// juan sebastian mandon sierra
 // 000426587
 // juan.mandon@upb.edu.co
 
 //SCREEN IS 16384 to 24575
 //KEYBOARD OR KBD IS 24576
-
-@SCREEN //Ubicacion en memoria de la pantalla
-D=A     //Guardo la ubicacion de screen en D
-@R1 //Mi contador, posicion R1 en memoria
-M=D //guardo el valor D en A (basicamente inicializo el valor de R1, mi contador, a 16384)
-
-(LLENARPANTALLALOOP)
-
-
-
-
-
 
 
 
