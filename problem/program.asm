c0d47bda8df8df7d4c5fc5b4807c48c340086751
LunarBullet <lunarbulletstudios@gmail.com>
2022-02-13 03:00:13 -0500

    no entiendo que le pasa al auto test si mi pantalla funciona

--- problem/program.asm
+++ problem/program.asm
@@ -1,12 +1,12 @@
 @SCREEN //Ubicacion en memoria de la pantalla
 D=A     //Guardo la ubicacion de screen en D
-@R1     //mi contador, posicion R1 en memoria
+@25     //mi contador, posicion R1 en memoria
 M=D     //guardo el valor D en A (basicamente inicializo el valor de R1, mi contador, a 16384)
 (LLENARPANTALLALOOP)
-@R1
+@25 
 A=M
 M=-1
-@R1
+@25
 M=M+1
 M=D
 @24575
